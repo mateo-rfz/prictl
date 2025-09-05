3abe3d0c40c022aecc59578f126981576a3cff57
mateo-rfz <mahdifeyzolahy@gmail.com>
2025-09-06 03:20:04 +0330

    add comment and helps

--- src/modules/mainCtrl.py
+++ src/modules/mainCtrl.py
@@ -1,8 +1,19 @@
+# use for press keyboard keys
 import pyautogui
+
+# use for find temporary dir for save qrcode picture
 import tempfile 
+
+# use for generate qrcode 
 import qrcode
+
+# use for open qrcode picture automatically
 import webbrowser
+
+# use for combine qrcode file name and store path
 import os
+
+# use for get local ip address
 import socket
 
 
@@ -12,7 +23,14 @@ class CTL :
         pass
 
 
-    def pressKey (self , row) : 
+    def pressKey (self , row) :
+        """
+        Press a keyboard key using the pyautogui module to navigate between slides.
+
+        Parameters:
+        row (str): 'next' to move to the next slide, any other value to move to the previous slide.
+        """
+
         if row == "next" : 
             pyautogui.press("right")
         else : 
@@ -20,10 +38,16 @@ class CTL :
 
 
 
-    def qr (self) :
+    def qrCreator (self , servicePort : int = 10001) :
+        """
+        Generate a QR code for a webpage URL based on the local IP and given port,
+        save it as 'prictl.png' in the system temporary directory, 
+        and automatically open it with the default image viewer
+        """
+
         try : 
             path = tempfile.gettempdir()
-            url = f"http://{getPrivateIp()}:8001"
+            url = f"http://{self.getPrivateIp()}:{servicePort}"
 
             file_path = os.path.join(path, f"prictl.png")
 
@@ -38,7 +62,11 @@ class CTL :
 
 
 
-    def getPrivateIp(self):
+    def getPrivateIp (self):
+        """
+        Return the local IP address of the machine by creating a UDP socket
+        and connecting to an external host (without sending data).
+        """
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
             s.connect(("8.8.8.8", 80))
