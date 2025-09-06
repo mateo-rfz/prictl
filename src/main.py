8ed26d016f84962b3714bbf020e48be541a974f6
mateo-rfz <mahdifeyzolahy@gmail.com>
2025-09-07 03:15:22 +0330

    add comment and help

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ from fastapi.exceptions import ResponseValidationError
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+
+
+# run fastapi app
 import uvicorn
 
 
@@ -25,27 +28,39 @@ templates = Jinja2Templates(directory="templates")
 
 
 
+
+
+
+
+
+
+
 @app.get("/")
-def main(
-    request: Request,
-    code_cookie: str = Cookie(None, alias="code"),
-    row: str = None,
-    code: str = None
-):
+def main(request: Request , code_cookie: str = Cookie(None, alias="code") , row: str = None , code: str = None):
+
+    #read generated pcode from file
     pcode = ctl.readCode()
 
     if code == pcode:
+        """
+        check code for auth and if valid 
+        app set coockie
+        """
         response = templates.TemplateResponse("home.html", {"request": request})
-        response.set_cookie(key="code", value=pcode, max_age=100000, path="/")
+        response.set_cookie(key="code", value= str(pcode) , max_age=100000, path="/")
         return response
 
-
+    
+    # check coockie value to pcode
     if code_cookie == pcode:
         if row in ("next", "prev"):
             ctl.pressKey(row)
         return templates.TemplateResponse("home.html", {"request": request})
 
+
     return templates.TemplateResponse("login.html", {"request": request})
+
+
 
 
 
@@ -60,12 +75,9 @@ def health () :
 
 
 
-
-
-
 if __name__ == "__main__" :
     ctl.randomCodeGenerator()
     pcode = ctl.readCode()
-    infoPageGenerator.Generaotr(pcode)
-    ctl.qrCreator(servicePort=10003)
-    uvicorn.run("main:app" , host="0.0.0.0" , port=10003 , reload=False)
+    infoPageGenerator.Generaotr(str(pcode))
+    ctl.qrCreator(servicePort=10001)
+    uvicorn.run("main:app" , host="0.0.0.0" , port=10001 , reload=False)
