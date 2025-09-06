c614acf61004856e65f6f523aaf55d9025672b77
mateo-rfz <mahdifeyzolahy@gmail.com>
2025-09-07 03:13:46 +0330

    add logger

--- src/modules/infoPageGenerator.py
+++ src/modules/infoPageGenerator.py
@@ -1,5 +1,11 @@
+# use for find temp dir on systems
 import tempfile
+
+# use for combine filename and path
 import os
+
+#local import 
+from modules import loger
 
 
 
@@ -7,17 +13,23 @@ import os
 
 class Generaotr  : 
     def __init__ (self , code : str = "1111") :
+        self.log = loger.Loger()
+
+        # read raw pageInfo from templates/info.html
         self.page = self.__readInfoPageFile()
+       
+        # pincode for auth
         self.code = code
-
-        path = tempfile.gettempdir()
-        self.path = os.path.join(path, "prictlInfo.html") 
-
-
-
-        self.qrPath = os.path.join(path , "prictl.png")
         
-        self.__gnr()
+
+        tempDirPath = tempfile.gettempdir()
+        self.tempDirPath = os.path.join(tempDirPath, "prictlInfo.html") 
+
+
+
+        self.qrPath = os.path.join(tempDirPath , "prictl.png")
+        
+        self.__generator()
         self.__saveReadyToUseInfoPage()
 
  
@@ -25,24 +37,36 @@ class Generaotr  :
 
 
     def __readInfoPageFile (self) : 
-        with open("templates/info.html" , "r") as file : 
-            file = file.read()
+        try : 
+            with open("templates/info.html" , "r") as file : 
+                file = file.read()
 
-        return file
+            return file
+
+        except Exception as e :
+            self.log.add("ERROR IN READ RAW INFO PAGE" , e)
+            return ""
 
 
 
     def __saveReadyToUseInfoPage (self) : 
-        with open(self.path , "w") as file : 
-            file.write(self.page)
+        try : 
+            with open(self.tempDirPath , "w") as file : 
+                file.write(self.page)
+
+            return True
+
+        except Exception as e : 
+            self.log.add("ERROR IN MAKE READY TO USE PAGE INFO" , e)
+            return False
         
 
 
 
 
 
-
-    def __gnr (self) :
+    
+    def __generator (self) :
         self.page = self.page.replace("%%%%" , self.code).replace("&&&&" , self.qrPath)
 
 
--- src/modules/mainCtrl.py
+++ src/modules/mainCtrl.py
@@ -21,9 +21,17 @@ import random
 
 
 
+# local imports 
+from modules import config
+from modules import loger
+
+
 class CTL : 
     def __init__ (self) : 
-        pass
+        self.log = loger.Loger()
+
+
+
 
 
     def pressKey (self , row) :
@@ -33,11 +41,23 @@ class CTL :
         Parameters:
         row (str): 'next' to move to the next slide, any other value to move to the previous slide.
         """
+        try : 
+            if row == "next" : 
+                pyautogui.press(config.NEXTKEY)
+                self.log.add("EMULATE PRESS KEY" , f"press {config.NEXTKEY}")
 
-        if row == "next" : 
-            pyautogui.press("right")
-        else : 
-            pyautogui.press("left")
+
+            else : 
+                pyautogui.press(config.PREVKEY)
+                self.log.add("EMULATE PRESS KEY" , f"press {config.PREVKEY}")
+
+
+        except Exception as e : 
+            self.log.add("ERROR IN EMULATE PRESS KEY" , e)
+            
+
+
+
 
 
 
@@ -49,22 +69,33 @@ class CTL :
         """
 
         try : 
-            path = tempfile.gettempdir()
+            tempDirPath = tempfile.gettempdir()
             url = f"http://{self.getPrivateIp()}:{servicePort}"
 
-            file_path = os.path.join(path, f"prictl.png")
+            qrCodePath = os.path.join(tempDirPath, f"prictl.png")
+            
 
             q = qrcode.make(url)
-            q.save(file_path)
+            q.save(qrCodePath)
+
+            self.log.add("SAVE QRCODE" , f"Path : {qrCodePath}")
 
 
-            infoPagePath = os.path.join(path, f"prictlInfo.html")
+            infoPagePath = os.path.join(tempDirPath, f"prictlInfo.html")
+
+            self.log.add("SAVE PAGE INFO" , f"Path {infoPagePath}")
+
+            #open default html viewer automatically
             webbrowser.open(infoPagePath)
-
+            
             return True
 
         except Exception as e : 
+            self.log.add("ERROR IN qrCreator()" , e) 
             return False
+
+
+
 
 
 
@@ -74,46 +105,73 @@ class CTL :
         and connecting to an external host (without sending data).
         """
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
         try:
-            s.connect(("8.8.8.8", 80))
-            ip = s.getsockname()[0]
+            s.connect(("8.8.8.8", 80)) 
+            ip = s.getsockname()[0] 
+
         finally:
             s.close()
+            
         return ip
 
 
 
 
 
+
+
+
+
     def randomCodeGenerator (self) : 
-        code = ""
-        for _ in range(4) : 
-            code = code + str(random.randint(0 , 9))
-
-        path = tempfile.gettempdir()
-        path = os.path.join(path, "priper_code.txt")
-        with open(path , "w") as file : 
-            file.write(code)
-
-
-        return code
+        """
+        Create random four-digit pincode to auth client by random module
+        store pincode on prictl_code.txt
+        """
+        try : 
+            code = ""
+            for _ in range(4) : 
+                code = code + str(random.randint(0 , 9))
 
 
 
+            path = tempfile.gettempdir()
+            path = os.path.join(path, "prictl_code.txt")
 
 
-    def readCode (self) : 
+            with open(path , "w") as file : 
+                file.write(code)
+            
+
+            self.log.add("SAVE PINCODE" , f"Path {path}")
+            return code
+
+        except Exception as e : 
+            self.log.add("ERROR IN GENERATE PINCODE" , e)
+
+
+
+
+
+
+
+
+    def readCode (self) :
+        """
+        Read pincode from prictl_code.txt
+        """
         try :   
             path = tempfile.gettempdir()
-            path = os.path.join(path, "priper_code.txt")
+            path = os.path.join(path, "prictl_code.txt")
 
             with open(path , "r") as file : 
                file = file.read() 
-
+            
+            self.log.add("READ PINCODE" , f"from {path}")
             return file
 
         except Exception as e : 
+            self.log.add("ERROR IN READ PINCODE" , e)
             return None
 
 
-        
